5b2e331b9bcb4a53623cfae33094d5174f9cf8db
torresposso <erictorres.co@gmail.com>
2025-09-01 15:21:56 -0500

    :sparkles: add lint workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   devServer: {
-    port:3001
+    port: 3001,
   },
-  modules: ['@nuxt/eslint'],
+  modules: ["@nuxt/eslint"],
   eslint: {
     config: {
-      standalone: false
-    }
-  }
-})
+      standalone: false,
+    },
+  },
+});
