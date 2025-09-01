02f786260df8b567a106fd72fa15df3bffedb66a
torresposso <erictorres.co@gmail.com>
2025-09-01 14:59:30 -0500

    :sparkles: setup eslint / lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port:3001
+  },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      standalone: false
+    }
+  }
 })
