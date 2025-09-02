44bb4c75fd6be332766f502c3e77c5a4bc15b0b0
torresposso <erictorres.co@gmail.com>
2025-09-02 18:57:39 -0500

    :sparkle: Setup auth | Resolve #15

--- app/db/schema/location-log.ts
+++ app/db/schema/location-log.ts
@@ -1,6 +1,6 @@
 import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-import { location } from ".";
+import { location, user } from ".";
 
 export const locationLog = sqliteTable("location_log", {
   id: integer().primaryKey({ autoIncrement: true }),
@@ -11,7 +11,7 @@ export const locationLog = sqliteTable("location_log", {
   latitude: real().notNull(),
   longitude: real().notNull(),
   locationId: integer().notNull().references(() => location.id),
-  userId: integer().notNull(),
+  userId: integer().notNull().references(() => user.id),
   createdAt: integer().notNull().$defaultFn(() => Date.now()),
   updatedAt: integer().notNull().$defaultFn(() => Date.now()).$onUpdate(() => Date.now()),
 });
--- app/db/schema/location.ts
+++ app/db/schema/location.ts
@@ -1,5 +1,7 @@
 import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from ".";
+
 export const location = sqliteTable("location", {
   id: integer().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
@@ -7,6 +9,7 @@ export const location = sqliteTable("location", {
   description: text().notNull(),
   latitude: real().notNull(),
   longitude: real().notNull(),
+  userId: integer("user_id").notNull().references(() => user.id),
   createdAt: integer("created_at").notNull().$defaultFn(() => Date.now()),
   updatedAt: integer("updated_at").notNull().$defaultFn(() => Date.now()).$onUpdate(() => Date.now()),
 });
