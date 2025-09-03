d49685b921a33f538700724d9a6f071692447479
Eric Torres Posso <erictorres.co@gmail.com>
2025-09-02 19:00:49 -0500

    :sparkle: Setup auth | Resolve #15

--- app/db/schema/location-log-image.ts
+++ app/db/schema/location-log-image.ts
@@ -1,11 +1,12 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-import { locationLog } from "./index";
+import { locationLog, user } from ".";
 
 export const locationLogImage = sqliteTable("location_log_image", {
   id: integer().primaryKey({ autoIncrement: true }),
   key: text().notNull(),
   locationLogId: integer().notNull().references(() => locationLog.id),
+  userId: integer().notNull().references(() => user.id),
   createdAt: integer().notNull().$defaultFn(() => Date.now()),
   updatedAt: integer().notNull().$defaultFn(() => Date.now()).$onUpdate(() => Date.now()),
 });
